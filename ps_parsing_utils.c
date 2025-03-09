596ee34692cbaa24b49cd9da7378e15a5f56fb8a
O-A5M <akhmouchothman@gmail.com>
2025-03-09 04:39:02 +0000

    the work continue in the parsing part

--- ps_parsing_utils.c
+++ ps_parsing_utils.c
@@ -46,6 +46,7 @@ int	ps_check_input(char *s)
 char	**splite_input(char **input, int ac)
 {
 	char	**ret;
+	char	**tmp;
 	int		i;
 
 	i = 0;
@@ -54,6 +55,11 @@ char	**splite_input(char **input, int ac)
 		return (NULL);
 	while (i < ac - 1)
 	{
+		if (ft_strchr(input[i], ' '))
+		{
+			tmp = ft_split(input, ' ');
+			join_2d(*ret, tmp);
+		}
 		ret[i] = malloc(sizeof (char) * (ft_strlen(input[i + 1]) + 1));
 		// if (!ret[i])
 			//TODO
@@ -74,4 +80,49 @@ void	free_array(char **array)
 		free(array[i]);
 		i++;
 	}
+}
+
+char	**join_2d(char **dest, char **src)
+{
+	char	**ret;
+	int		len;
+	int		i;
+	int		j;
+
+	i = 0;
+	len = array_len(dest) + array_len(src);
+	ret = malloc(sizeof(char *) * (len + 1));
+	if (!ret)
+		return ;
+	while (dest[i])
+	{
+		ret[i] = malloc(sizeof(dest[i]));
+		if (!ret[i])
+			return ;
+		ret[i] = dest[i];
+		i++;
+	}
+	j = 0;
+	while (src[j])
+	{
+		ret[i] = malloc(sizeof(src[j]));
+		if (!ret[i])
+			return ;
+		ret[i] = src[j];
+		i++;
+		j++;
+	}
+	ret[i] = NULL;
+	return (ret);
+}
+
+int	array_len(char **arr)
+{
+	int	ret;
+
+	while (arr[ret])
+	{
+		ret++;
+	}
+	return (ret);
 }
